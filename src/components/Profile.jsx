07b481696e88253392c8fd0771036cf9833610ea
Бессонова Вероника Алексеевна <vbessonova@phoenixit.ru>
2021-02-12 15:43:37 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <div className="app-content">
+      Main content
+      <img src='https://i.pinimg.com/originals/cd/98/4d/cd984ddfa74df2014937b187455e8d2d.jpg'/>
+      <div>
+        ava+discription
+        <img src='https://lh3.googleusercontent.com/proxy/IOpsEl_6Jw5qdSQeZsof7fSJnTXdsCyqDp6YgH5EckA4e5bIdGAtv_0Mld88lTB8ChRBi3xlK0tl4aVmUWmjyTfNkHaJPJmlFKYDrpDR845Cwd1pucfKfT7TRgqKlKpqJ94TmEbZSjNRIIIysDO0tVKM'/>
+
+      </div>
+      <div>
+        My post
+        <div>New Post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
