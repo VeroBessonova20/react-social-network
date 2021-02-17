ac49d7f3ee80332004dc0778d8556a98d1a30847
Veronika <vbessonova@fil-it.ru>
2021-02-17 12:44:39 +0300

    add module

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
+import cls from './Navbar.module.css';
+
 const Navbar = () => {
   return (
-    <nav className="app-nav">
-      <div><a>Profile</a></div>
-      <div><a>Messages</a></div>
-      <div><a>News</a></div>
-      <div><a>Music</a></div>
-      <div><a>Books</a></div>
-      <div><a>Films</a></div>
-      <div><a>Settings</a></div>
+    <nav className={cls.nav}>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Profile</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Messages</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">News</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Music</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Books</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Films</a>
+      </div>
+      <div className={`${cls.item} ${cls.active}`}>
+        <a href="/">Settings</a>
+      </div>
     </nav>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,20 @@
+import classes from './Profile.module.css';
+
 const Profile = () => {
   return (
-    <div className="app-content">
-      Main content
+    <div className={classes.content}>
       <img src='https://i.pinimg.com/originals/cd/98/4d/cd984ddfa74df2014937b187455e8d2d.jpg'/>
       <div>
         ava+discription
         <img src='https://lh3.googleusercontent.com/proxy/IOpsEl_6Jw5qdSQeZsof7fSJnTXdsCyqDp6YgH5EckA4e5bIdGAtv_0Mld88lTB8ChRBi3xlK0tl4aVmUWmjyTfNkHaJPJmlFKYDrpDR845Cwd1pucfKfT7TRgqKlKpqJ94TmEbZSjNRIIIysDO0tVKM'/>
 
       </div>
-      <div>
+      <div className={classes.post}>
         My post
-        <div>New Post</div>
-        <div>Post 1</div>
-        <div>Post 2</div>
-        <div>Post 3</div>
+        <div className={classes.item}>New Post</div>
+        <div className={classes.item}>Post 1</div>
+        <div className={classes.item}>Post 2</div>
+        <div className={classes.item}>Post 3</div>
       </div>
     </div>
   )
